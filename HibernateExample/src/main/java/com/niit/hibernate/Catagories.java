c86a8f3e4a7bce43eb0edcf467309cf9d2202416
shruthichethangowda <shruthichethangowda@gmail.com>
2016-12-29 16:24:51 +0530

    hibernate
    hibernate using xml

---
+++ HibernateExample/src/main/java/com/niit/hibernate/Catagories.java
@@ -0,0 +1,19 @@
+package com.niit.hibernate;
+
+public class Catagories {
+	private int CatagoryId;
+	private String CatagoryName;
+	public int getCatagoryId() {
+		return CatagoryId;
+	}
+	public void setCatagoryId(int catagoryId) {
+		CatagoryId = catagoryId;
+	}
+	public String getCatagoryName() {
+		return CatagoryName;
+	}
+	public void setCatagoryName(String catagoryName) {
+		CatagoryName = catagoryName;
+	}
+
+}
